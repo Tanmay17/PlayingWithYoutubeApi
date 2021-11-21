eef050e4445ef5a1990475a1fd9cd0a2e5366181
Tanmay <tanmay.srivastava@biofourmis.com>
2021-11-21 14:33:25 +0530

    chore: moved mongo config to .env

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ mongoose
 
     app.use("/", routes);
     app.listen(3000, async () => {
-      await mongoose.connect("mongodb://localhost:27017/fp-assignment");
+      await mongoose.connect(process.env.MONGO_URL);
 
       cron.schedule(process.env.CRON_SCHEDULE, async () => {
         console.log("Scheduling Cron");
